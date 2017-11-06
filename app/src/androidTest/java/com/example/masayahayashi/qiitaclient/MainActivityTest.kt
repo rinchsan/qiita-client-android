df854a96a6f82124f065296cfa1b6c1cbf27a346
Masaya Hayashi <mcdonalds.only@gmail.com>
2017-11-07 02:07:56 +0900

    Add ui test for searching

--- app/src/androidTest/java/com/example/masayahayashi/qiitaclient/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/masayahayashi/qiitaclient/MainActivityTest.kt
@@ -1,17 +1,22 @@
 package com.example.masayahayashi.qiitaclient
 
 import android.support.test.espresso.Espresso.onView
+import android.support.test.espresso.action.ViewActions.click
+import android.support.test.espresso.action.ViewActions.typeText
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
 import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import android.view.View
+import com.example.masayahayashi.qiitaclient.client.ArticleClient
+import io.reactivex.Observable
 import org.hamcrest.Matcher
 import org.hamcrest.Matchers.not
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.Mockito.*
 
 /**
  * Created by masayahayashi on 2017/11/07.
@@ -34,5 +39,18 @@ class MainActivityTest {
     }
 
     fun isNotDisplayed(): Matcher<View> = not(isDisplayed())
+
+    @Test
+    fun `検索ボタンがタップされたら、入力されたクエリ文字列で記事検索APIを叩くこと`() {
+        val articleClient = mock(ArticleClient::class.java).apply {
+            `when`(search("user:snowman_mh")).thenReturn(Observable.just(listOf()))
+        }
+        activityTestRule.activity.articleClient = articleClient
+
+        onView(withId(R.id.query_edit_text)).perform(typeText("user:snowman_mh"))
+        onView(withId(R.id.search_button)).perform(click())
+
+        verify(articleClient).search("user:snowman_mh")
+    }
 
 }
